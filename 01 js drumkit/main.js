7dcb4c93189af27444d0d1ef21e18785e7ff4bf1
amries grace <obieri.grace@gmail.com>
2017-08-09 17:07:22 +0100

    javascript drumkit

---
+++ 01 js drumkit/main.js
@@ -0,0 +1,25 @@
+
+function playSound(e){
+	//console.log(e.keyCode);
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	//console.log(audio);
+	if(!audio) 
+		return // stops function from running all together
+	audio.currentTime = 0; //rewind to start
+	audio.play();
+
+	key.classList.add('playing');
+	//console.log(key);
+}
+
+
+function removeTransition(e){
+	if(e.propertyName !== 'transform')
+		return;
+	this.classList.remove('playing');
+	//console.log(e.propertyName);
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound) ;
